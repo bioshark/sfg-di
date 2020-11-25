5555ffdaac6eed51523f4b6e8696103f13d1a276
bioshark <bioshark.dev@gmail.com>
2020-11-25 20:06:00 +0100

    Using profiles on beans.

--- src/main/java/roly/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/roly/springframework/sfgdi/SfgDiApplication.java
@@ -3,34 +3,32 @@ package roly.springframework.sfgdi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import roly.springframework.sfgdi.controllers.ConstructorInjectedController;
-import roly.springframework.sfgdi.controllers.MyController;
-import roly.springframework.sfgdi.controllers.PropertyInjectedController;
-import roly.springframework.sfgdi.controllers.SetterInjectedController;
+import roly.springframework.sfgdi.controllers.*;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-        MyController myController = (MyController) ctx.getBean("myController");
+
+        System.out.println("------------------------ PROFILE BASED GREETING ------------------");
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
 
         System.out.println("------------------------ PRIMARY ------------------");
+        MyController myController = (MyController) ctx.getBean("myController");
         System.out.println(myController.sayHello());
 
         System.out.println("------------------------ PROPERTY ------------------");
         PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
         System.out.println(propertyInjectedController.getGreeting());
 
         System.out.println("------------------------ SETTER ------------------");
         SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
         System.out.println(setterInjectedController.getGreeting());
 
         System.out.println("------------------------ CONSTRUCTOR ------------------");
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
         System.out.println(constructorInjectedController.getGreeting());
 
 //        System.out.println("------------AVAILABLE BEANS ------------");
