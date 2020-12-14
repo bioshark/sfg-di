58aa3e6352bd3473fe03cf063301e61cca33033b
bioshark <bioshark.dev@gmail.com>
2020-12-14 21:02:06 +0100

    playing with environmental variables.

--- src/main/java/roly/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/roly/springframework/sfgdi/config/PropertyConfig.java
@@ -1,14 +1,19 @@
 package roly.springframework.sfgdi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import roly.springframework.sfgdi.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    private Environment env;
 
     @Value("${roly.username}")
     String user;
@@ -20,7 +25,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+//        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
